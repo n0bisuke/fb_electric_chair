8c067cd9d8a2bba08b58c7f0bddd09fe762b6a7d
のびすけ <rsksound@gmail.com>
2017-05-21 17:19:18 +0900

    add express

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 'use strict';
 
+const express = require('express');
+const app = express();
 const axios = require(`axios`);
 const FBURL = `https://www.facebook.com/burghamburgburg/`;
+const PORT = process.env.PORT || 3000;
 
-axios.get(FBURL)
-  .then((response) => {
-    const html = response.data;
-    const like = html.match(/#xff01;(.*?)&#x4ef6;/)[1];
-    console.log(like);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+app.get('/', (req, res) => {
+
+    axios.get(FBURL)
+    .then((response) => {
+        const html = response.data;
+        const like = html.match(/#xff01;(.*?)&#x4ef6;/)[1];
+        console.log(like);
+        res.send(`${FBURL}のいいね数は${like}です。`);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+});
+ 
+app.listen(3000);
