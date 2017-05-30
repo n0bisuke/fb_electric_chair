0e9592d80119e05f9151ae17c145697f46596249
のびすけ <rsksound@gmail.com>
2017-05-31 02:50:00 +0900

    FBログチェック

--- server.js
+++ server.js
@@ -6,20 +6,36 @@ const axios = require(`axios`);
 const FBURL = `https://www.facebook.com/burghamburgburg/`;
 const PORT = process.env.PORT || 3000;
 
+let prevLike = 0;
+let flag = 0;
+
 app.get('/', (req, res) => {
 
     axios.get(FBURL)
     .then((response) => {
         const html = response.data;
         const like = html.match(/#xff01;(.*?)&#x4ef6;/)[1];
-        console.log(like);
+        const likeNum = parseInt(like.replace(/,/g,'')); //文字列 -> 数値に変換
+        const mes = {};
+        mes.diff = `前回アクセス時のいいね数: ${prevLike}, 今回のいいね数${likeNum}`;
+        mes.log = `${FBURL}のいいね数は${like}です。`;
+
+        if((prevLike !== 0) && (prevLike < likeNum)){
+            flag = 1;
+            mes.status = `いいねあり`;
+        }else{
+            flag = 0;
+            mes.status = `変化なし`;
+        }
+
         const result = {
-            like: like,
-            mes: `${FBURL}のいいね数は${like}です。`
+            new: flag,
+            like: likeNum,
+            mes: mes
         };
-        
         res.contentType('application/json');
         res.send(result);
+        prevLike = likeNum;
     })
     .catch((error) => {
         console.log(error);
